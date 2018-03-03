20d797af09a257ff863caeb86052a07f9793abfc
Nawar Yossef <nawaryossef2@gmail.com>
2018-03-02 22:32:33 -0500

    add cors function

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const {CLIENT_ORIGIN} = require('./config');
 
 const PORT = process.env.PORT || 8080;
 
 // CORS
-app.use(
-  cors({
-      origin: CLIENT_ORIGIN
-  })
-);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+  next();
+});
 
 app.get("/api/user/*", (req , res) => {
   res.status(200).json({ok: true})
