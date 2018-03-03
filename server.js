ce91936b8a00a5d69771d640bb427a2bd04ad418
Nawar Yossef <nawaryossef2@gmail.com>
2018-03-02 22:46:30 -0500

    reinstall and add cors package

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const express = require("express");
 const app = express();
-// const cors = require('cors');
-// const {CLIENT_ORIGIN} = require('./config');
+const cors = require("cors");
+const { CLIENT_ORIGIN } = require("./config");
 
 const PORT = process.env.PORT || 8080;
 
 // CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
-  );
-  next();
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+
+app.get("/api/user/*", (req, res) => {
+  res.status(200).json({ ok: true });
 });
 
-app.get("/api/user/*", (req , res) => {
-  res.send("hi")
-})
-
 app.listen(PORT, () => {
-  console.log(`Your app is listening on port ${PORT}`)
-})
+  console.log(`Your app is listening on port ${PORT}`);
+});
 
-module.exports = {app}
+module.exports = { app };
